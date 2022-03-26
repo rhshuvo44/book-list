420a667c364079425ff6cb7eccda0de92856e128
Ripon Hossain Shuvo <rhshuvo44@gmail.com>
2022-03-26 13:07:49 +0600

    ans

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import BookList from './component/BookList/BookList';
+import Question from './component/Question/Question';
 
 function App() {
   return (
     <div>
       <BookList></BookList>
+      <Question></Question>
     </div>
   );
 }
